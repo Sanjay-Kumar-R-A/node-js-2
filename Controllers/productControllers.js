c7323882687e186497a26a9ae71b442b37af3041
Sanjay-Kumar-R-A <rasanjaykumarsk@gmail.com>
2024-11-20 16:13:45 +0530

    put/delete

--- Controllers/productControllers.js
+++ Controllers/productControllers.js
@@ -1,7 +1,10 @@
 const products = [
     {id:1,name:"iphone 14",price:70000},
     {id:2,name:"iphone 15",price:80000},
-    {id:3,name:"iphone 16",price:90000}  
+    {id:3,name:"iphone 16",price:90000},
+    {id:4,name:"iphone 17",price:100000},
+    {id:5,name:"iphone 18",price:110000},
+    
 ]
 
 //get method
@@ -36,5 +39,37 @@ export const createProduct = (req,res) => {
 
     products.push(newProduct);
     res.status(201).json({data:newProduct,message:"product created successfully"});
-    
+
 }
+
+//put method
+export const updateProduct = (req,res) => {
+    const productId = req.params.id;
+    const {name,price} = req.body;
+
+    const productdetail = products.find((ele) => ele.id == productId);
+
+    if(productdetail){
+        productdetail.name = name;
+        productdetail.price = price;
+        res.status(200).json({data:productdetail,message:"product updated successfully"});
+    }else{
+        res.status(404).json({message:"product not found"});
+    }
+}
+
+//delete method
+export const deleteProduct = (req,res) => {
+    const productId = req.params.id;
+    const index = products.find((ele) => ele.id == productId);
+
+    if(index ===-1){  
+        return res.status(404).json({message:"product not found"}); 
+    }  
+        
+        products.splice(index,1);    
+        res.status(200).json({message:"product deleted successfully"});
+    };
+    
+        
+    
--- Routers/productRouters.js
+++ Routers/productRouters.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createProduct, getProductById, getProducts } from "../Controllers/productControllers.js";
+import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from "../Controllers/productControllers.js";
 
 const router = express.Router();
 
 router.get("/getdata",getProducts);
 router.get("/getdata/:id",getProductById);
 router.post("/create",createProduct);
+router.put("/update/:id",updateProduct);
+router.delete("/delete/:id",deleteProduct);
 
 export default router;
